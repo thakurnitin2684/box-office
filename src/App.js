3f2fb24ff427010f1a5dccffc65c283dd47532a3
thakurnitin2684 <thakurnitin2684@gmail.com>
2021-04-14 17:22:00 +0530

    Styles show page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
@@ -14,7 +15,14 @@ function App() {
       <Route exact path="/starred">
         <Starred />
       </Route>
-      <Route>Not Found</Route>
+
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+
+      <Route>
+        <div>Not found</div>
+      </Route>
     </Switch>
   );
 }
